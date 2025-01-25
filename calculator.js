0e334edb4f81396567852409ec38d1624ea8075d
Jessica Ulrich <jessicaanne0982@gmail.com>
2025-01-25 03:05:38 -0500

    These are the files for my Module 10, Lesson 1 assignment on JavaScript Basics and Syntax

---
+++ calculator.js
@@ -0,0 +1,48 @@
+// Task 1 - declare variables
+let num1;
+let num2;
+
+// Task 2 - assign variables
+num1 = 13;
+num2 = 20;
+
+// Task 3 - perform arithmetic operations and display results
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+// Task 4 - Explore assignment operators  and update values
+
+num1 -= 3;
+num2 /= 2;
+
+console.log("Updated num:", num1);
+console.log("Updated num2:", num2);
+
+// Task 5 - Use comparison operators to compare values
+
+let equalTo = num1 == num2;
+let greaterThan = num1 > num2;
+let notEqual = num1 != num2;
+
+console.log("Is num1 equal to num2?", equalTo);
+console.log("Is num1 greater than num2?", greaterThan);
+console.log("Is num1 not equal to num2?", notEqual);
+
+// Task 6 - Apply logical operators to combine conditions and display
+ 
+if (num1 >=0 && num2 >=0);
+    let isPositive = true;
+ 
+if (num1 % 2 == 0 || num2 % 2 == 0);
+    let isEven = true;
+ 
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
---
+++ school.js
@@ -0,0 +1,20 @@
+// Task 1 - Declare variables
+
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+// Task 2 - Assign values to variables
+
+studentName = "Suzie Summers";
+studentAge = 18;
+studentGrade = 12;
+isPresent = false;
+
+// Task 3 - Display student information to the console
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Is Student Present?", isPresent);
